72065e198c12821c0e56eccb02cabe04f9472782
yfigueroa <yfsanchez@gmail.com>
2021-01-24 19:03:25 -0300

    Section_22:API REST
    Section_22:API REST

--- src/main/java/com/yfsanchez/springboot/app/SpringSecuryConfig.java
+++ src/main/java/com/yfsanchez/springboot/app/SpringSecuryConfig.java
@@ -37,7 +37,7 @@ public class SpringSecuryConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/images/**","/listar**","/locale").permitAll()
+		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/images/**","/listar**","/locale","/api/clientes/**").permitAll()
 //		.antMatchers("/ver/**").hasAnyRole("USER")
 //		.antMatchers("/uploads/**").hasAnyRole("USER")
 //		.antMatchers("/form/**").hasAnyRole("ADMIN")
